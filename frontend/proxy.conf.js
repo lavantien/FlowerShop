f4b09323636d580a639a30e931b6435d60df3f2e
lavantien <lavantien@users.noreply.github.com>
2026-01-01 05:22:33 +0700

    chore: sync system instructions

--- frontend/proxy.conf.js
+++ frontend/proxy.conf.js
@@ -1,18 +1,16 @@
 const PROXY_CONFIG = [
-	{
-		context: [
-			'/api'
-		],
-		target: 'http://localhost:8080',
-		secure: false,
-		changeOrigin: true,
-		logLevel: 'debug',
-		headers: {
-			host: 'localhost'
-		},
-		cookieDomainRewrite: {
-			'localhost': 'localhost'
-		}
-	}
+  {
+    context: ["/api"],
+    target: "http://localhost:8080",
+    secure: false,
+    changeOrigin: true,
+    logLevel: "debug",
+    headers: {
+      host: "localhost",
+    },
+    cookieDomainRewrite: {
+      localhost: "localhost",
+    },
+  },
 ];
 module.exports = PROXY_CONFIG;
